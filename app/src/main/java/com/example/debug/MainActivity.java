4d7c03797f6e0877761d9c2dfd1758cc6a96d873
BaeJunBeom <qogodls1>
2019-11-26 14:45:18 +0900

    Initial commit

---
+++ app/src/main/java/com/example/debug/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("ACTIVITY_LC", "onCreate 호출됨");
+        Toast.makeText(getApplicationContext(), "onCreate 호출됨", Toast.LENGTH_SHORT);
+        Button button = (Button) findViewById(R.id.newActivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SubActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/debug/SubActivity.java
@@ -0,0 +1,31 @@
+package com.example.debug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SubActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+        Log.d("ACTIVITY_LC", "onCreate 호출됨");
+        Toast.makeText(getApplicationContext(), "onCreate 호출됨", Toast.LENGTH_SHORT);
+        Button button = (Button) findViewById(R.id.subactivity);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Three.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
+

0f91cac9729de5a54d007f3597b8febaedb7473f
BaeJunBeom <qogodls1>
2019-11-27 17:08:49 +0900

    4번시도

--- app/src/main/java/com/example/debug/Five.java
+++ app/src/main/java/com/example/debug/Five.java
@@ -9,22 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Three extends AppCompatActivity {
-// 3번째 커밋*
+public class Five extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
+        setContentView(R.layout.activity_five);
         Log.d("ACTIVITY_LC", "onCreate 호출됨");
         Toast.makeText(getApplicationContext(), "onCreate 호출됨", Toast.LENGTH_SHORT);
-        Button button = (Button) findViewById(R.id.three);
+        Button button = (Button) findViewById(R.id.fiveactivity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Four.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+
             }
         });
     }
 }
-
--- app/src/main/java/com/example/debug/Four.java
+++ app/src/main/java/com/example/debug/Four.java
@@ -9,22 +9,23 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class Three extends AppCompatActivity {
-// 3번째 커밋*
+public class Four extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_three);
+        setContentView(R.layout.activity_four);
         Log.d("ACTIVITY_LC", "onCreate 호출됨");
         Toast.makeText(getApplicationContext(), "onCreate 호출됨", Toast.LENGTH_SHORT);
-        Button button = (Button) findViewById(R.id.three);
+        Button button = (Button) findViewById(R.id.foractivity);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Four.class);
+                Intent intent = new Intent(getApplicationContext(), Five.class);
                 startActivity(intent);
             }
         });
     }
 }
+
 
